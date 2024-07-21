e6c4b13739d32c3802bcef4ce3b64420cb06126b
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-07-21 12:52:29 -0400

    feat: add inclusao de arquivos

---
+++ 8_inclusao_de_arquivo/5_exibicao_de_conteudo/index.php
@@ -0,0 +1,18 @@
+<?php
+
+  $nome = "Tales";
+  $sobrenome = "Nascimento";
+
+?>
+
+<form action="">
+  <div>
+    <input type="text" value="<?= $nome; ?>">
+  </div>
+  <div>
+    <input type="text" value="<?= $sobrenome; ?>">
+  </div>
+  <div>
+    <input type="submit" value="Enviar">
+  </div>
+</form>
